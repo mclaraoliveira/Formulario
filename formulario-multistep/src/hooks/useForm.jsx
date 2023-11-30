e64f2fccc66501e233dd59bf433d318408f0f8ff
Clara Oliveira <122572905+mclaraoliveira@users.noreply.github.com>
2023-11-30 19:11:31 -0300

    Mais configurações dos botões

--- formulario-multistep/src/hooks/useForm.jsx
+++ formulario-multistep/src/hooks/useForm.jsx
@@ -17,5 +17,7 @@ export function useForm(steps) {
         currentStep,
         currentComponent: steps[currentStep],
         changeStep,
+        isLastStep: currentStep + 1 === steps.length ? true : false,
+        isFirstStep: currentStep === 0 ? true : false,
     }
 } 
