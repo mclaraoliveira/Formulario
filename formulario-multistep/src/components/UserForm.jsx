edc03df643bb06144834dc884e29c0b5cf495023
Clara Oliveira <122572905+mclaraoliveira@users.noreply.github.com>
2023-11-30 19:41:31 -0300

    Finalização do CSS inicial

--- formulario-multistep/src/components/UserForm.jsx
+++ formulario-multistep/src/components/UserForm.jsx
@@ -3,7 +3,14 @@ import React from "react";
 const UserForm = () => {
     return (
         <div>
-            <h2>UserForm</h2>
+            <div className="form-control">
+                <label htmlFor="name">Nome:</label>
+                <input type="text" name="text" id="name" placeholder="Digite o seu nome:" required />
+            </div>
+            <div className="form-control">
+                <label htmlFor="email">E-mail:</label>
+                <input type="email" name="email" id="email" placeholder="Digite o seu e-mail:" required />
+            </div>
             </div>
     );
 };
