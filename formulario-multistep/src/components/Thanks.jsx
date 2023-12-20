f38381b243853aca9a7c74c44256a91868160a50
Clara Oliveira <122572905+mclaraoliveira@users.noreply.github.com>
2023-12-19 23:15:05 -0300

    Tentando resolver o problema com a caixa para email na última página mas ainda não consegui

--- formulario-multistep/src/components/Thanks.jsx
+++ formulario-multistep/src/components/Thanks.jsx
@@ -2,55 +2,60 @@ import React, { useState } from "react";
 import "./Thanks.css";
 
 const Thanks = ({ data, updateFieldHandler }) => {
-    const [querReceberNewsletter, setQuerReceberNewsletter] = useState(false);
+  const [querReceberNewsletter, setQuerReceberNewsletter] = useState(false);
 
-    const handleNewsletterChange = (value) => {
-        setQuerReceberNewsletter(value);
-        updateFieldHandler("newsletter", value);
-    };
+  const handleNewsletterChange = (value) => {
+    setQuerReceberNewsletter(value);
+    updateFieldHandler("newsletter", value);
 
-    return (
-        <div className="thanks-container">
-            <h2>Obrigado pelo cadastro para uso da SoftexLabs, retornaremos em breve</h2>
+    // Limpar o campo de e-mail se o usuário optar por não receber o newsletter
+    if (!value) {
+      updateFieldHandler("emailNewsletter", ""); // Limpa o campo de e-mail
+    }
+  };
 
-            <div className="form-control">
-                <label>Deseja receber nosso newsletter?</label>
-                <div>
-                    <label>
-                        <input
-                            type="radio"
-                            name="newsletter"
-                            value="sim"
-                            checked={querReceberNewsletter}
-                            onChange={() => handleNewsletterChange(true)}
-                        />
-                        Sim
-                    </label>
-                    <label>
-                        <input
-                            type="radio"
-                            name="newsletter"
-                            value="nao"
-                            checked={!querReceberNewsletter}
-                            onChange={() => handleNewsletterChange(false)}
-                        />
-                        Não
-                    </label>
-                </div>
-            </div>
+  return (
+    <div className="thanks-container">
+      <h2>Obrigado pelo cadastro para uso da SoftexLabs, retornaremos em breve</h2>
 
-            {querReceberNewsletter && (
-                <div className="form-control">
-                    <label>Digite seu e-mail:</label>
-                    <input
-                        type="text"
-                        value={data.emailNewsletter || ""}
-                        onChange={(e) => updateFieldHandler("emailNewsletter", e.target.value)}
-                    />
-                </div>
-            )}
+      <div className="form-control">
+        <label>Deseja receber nosso newsletter?</label>
+        <div>
+          <label>
+            <input
+              type="radio"
+              name="newsletter"
+              value="sim"
+              checked={querReceberNewsletter}
+              onChange={() => handleNewsletterChange(true)}
+            />
+            Sim
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="newsletter"
+              value="nao"
+              checked={!querReceberNewsletter}
+              onChange={() => handleNewsletterChange(false)}
+            />
+            Não
+          </label>
         </div>
-    );
+      </div>
+
+      {querReceberNewsletter && (
+        <div className="form-control">
+          <label>Digite seu e-mail:</label>
+          <input
+            type="text"
+            value={data.emailNewsletter || ""}
+            onChange={(e) => updateFieldHandler("emailNewsletter", e.target.value)}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Thanks;
